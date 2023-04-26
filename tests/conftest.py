6986505e525448f832b4c55f5e6ac3fc8d6b2451
jgfranco17 <chino.franco@gmail.com>
2023-04-27 01:11:12 +0900

    Added fixture for mock data

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,13 +2,13 @@ import sys
 import pytest
 
 
-# each test runs on cwd to its temp dir
-@pytest.fixture(autouse=True)
-def go_to_tmpdir(request):
-    # Get the fixture dynamically by its name.
-    tmpdir = request.getfixturevalue("tmpdir")
-    # ensure local test created packages can be imported
-    sys.path.insert(0, str(tmpdir))
-    # Chdir only for the duration of the test.
-    with tmpdir.as_cwd():
-        yield
+@pytest.fixture(scope="module")
+def mock_weather_data():
+    yield {
+        "city": "New York",
+        "temperature": 15,
+        "description": "Cloudy",
+        "humidity": 60,
+        "wind_speed": 10,
+        "pressure": 1015
+    }
