4664b77ed561d5061ff33e1e372684e3b5292804
jgfranco17 <chino.franco@gmail.com>
2023-04-27 01:04:28 +0900

    Removed CLI entrypoint from setup

--- setup.py
+++ setup.py
@@ -38,8 +38,5 @@ setup(
     author="jgfranco17",
     packages=find_packages(exclude=["tests", ".github"]),
     install_requires=read_requirements("requirements.txt"),
-    entry_points={
-        "console_scripts": ["pyclima = pyclima.__main__:main"]
-    },
     extras_require={"test": read_requirements("requirements-test.txt")},
 )
