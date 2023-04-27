f26c300d9074d799a4077e0480feb6d9b6f9b637
jgfranco17 <chino.franco@gmail.com>
2023-04-27 11:33:49 +0900

    Added method for historical data

--- pyclima/forecast.py
+++ pyclima/forecast.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 class Forecaster:
     BASE_URL = "https://api.open-meteo.com/v1"
     
-    def __init__(self, latitude:float, longitude:float, span:int=7) -> None:
+    def __init__(self, latitude:float, longitude:float) -> None:
         hourly_metrics = [
             "temperature_2m",
             "apparent_temperature",
@@ -32,3 +32,21 @@ class Forecaster:
     @property
     def coordinates(self) -> tuple:
         return self.__latitude, self.__longitude
+    
+    def get_historical(self, span:int=7) -> pd.DataFrame:
+        # Get API request response and decode JSON data
+        try:
+            weather_data = requests.get(f'{self.__url}&past_days={span}').json()
+            raw_hourly_data = weather_data["hourly"]
+
+            # Preparing JSON data for dataframe conversion
+            hourly_timestamps = [dt.datetime.strptime(t, "%Y-%m-%dT%H:%M") for t in raw_hourly_data["time"]]
+            format_string = lambda s: s.replace("_2m", "")
+            hourly_data = {format_string(key): series for key, series in raw_hourly_data.items() if key != "time"}
+
+            # Convert data to dataframe
+            df = pd.DataFrame(hourly_data, index=hourly_timestamps)
+            
+        except requests.JSONDecodeError as jde:
+            print(f'Failed to decode JSON data: {jde}')
+            return None
